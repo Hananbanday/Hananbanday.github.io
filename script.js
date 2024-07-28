372a814582820a90610df37b45fed48a75d1602c
Hananbanday <122731896+Hananbanday@users.noreply.github.com>
2024-07-28 19:48:34 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', (event) => {
+    const form = document.getElementById('contact-form');
+    
+    form.addEventListener('submit', (e) => {
+        e.preventDefault();
+        alert('Thank you for your message! I will get back to you soon.');
+        form.reset();
+    });
+});
